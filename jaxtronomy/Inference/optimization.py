5805d54984eb6ad22b26a223fdf43bb51718ec82
Aymeric Galan <aymeric.galan@gmail.com>
2021-06-25 16:34:34 +0200

    Fix support for 'trust-constr' minimization method

--- jaxtronomy/Inference/optimization.py
+++ jaxtronomy/Inference/optimization.py
@@ -33,21 +33,21 @@ class Optimizer(InferenceBase):
             raise ValueError("You must run the optimizer at least once to access the history")
         return self._metrics.param_history
 
-    def minimize(self, method='BFGS', restart_from_init=False, exact_hessian_if_allowed=False):
+    def minimize(self, method='BFGS', restart_from_init=False, use_exact_hessian_if_allowed=False):
         # TODO: should we call once / a few times all jitted functions before optimization, to potentially speed things up?
-        if method not in self._supported_scipy_methods:
-            raise ValueError(f"Minimize method '{method}' is not supported.")
         init_params = self._param_class.initial_values(as_kwargs=False, original=restart_from_init)
-        self._metrics = Metrics(self.loss)
+        self._metrics = MinimizeMetrics(self.loss, method)
         start = time.time()
         best_fit, extra_fields = self._run_scipy_minimizer(init_params, method, self._metrics,
-                                                           exact_hessian_if_allowed)
+                                                           use_exact_hessian_if_allowed)
         runtime = time.time() - start
         logL_best_fit = - float(self._metrics.loss_history[-1])
         self._param_class.set_best_fit(best_fit)
         return best_fit, logL_best_fit, extra_fields, runtime
 
     def _run_scipy_minimizer(self, x0, method, callback, exact_hessian):
+        if method not in self._supported_scipy_methods:
+            raise ValueError(f"Minimize method '{method}' is not supported.")
         # here we only put select the kwargs related to the chosen method 
         extra_kwargs = {}
         if method in ['BFGS']:
@@ -59,7 +59,7 @@ class Optimizer(InferenceBase):
             else:
                 extra_kwargs['hessp'] = self.hessian_vec_prod
             if method == 'trust-constr':
-                extra_kwargs['bounds'] = self._scipy_bounds
+                extra_kwargs['bounds'] = Bounds(*self._param_class.bounds)
         opt = optimize.minimize(self.loss, x0, method=method, callback=callback, 
                                 **extra_kwargs)
         extra_fields = {'jac': None, 'hess': None, 'hess_inv': None}
@@ -68,14 +68,14 @@ class Optimizer(InferenceBase):
                 extra_fields[key] = getattr(opt, key)
         return opt.x, extra_fields
 
-    @property
-    def _scipy_bounds(self):
-        if not hasattr(self, '_bounds'):
-            lowers, uppers = self._param_class.bounds
-            assert np.any(np.isnan(lowers)) is False, "NaNs found in lower bounds, please check uniform prior bounds"
-            assert np.any(np.isnan(uppers)) is False, "NaNs found in upper bounds, please check uniform prior bounds"
-            self._bounds = Bounds(lowers, uppers)
-        return self._bounds
+    # @property
+    # def _scipy_bounds(self):
+    #     if not hasattr(self, '_bounds'):
+    #         lowers, uppers = 
+    #         assert np.all(np.isfinite(lowers)), "NaNs found in lower bounds, please check uniform prior bounds"
+    #         assert np.all(np.isfinite(uppers)), "NaNs found in upper bounds, please check uniform prior bounds"
+    #         self._bounds = 
+    #     return self._bounds
 
     def pso(self, n_particles=100, n_iterations=100, restart_from_init=False, n_threads=1):
         """legacy optimization method from lenstronomy, mainly for comparison"""
@@ -98,12 +98,27 @@ class Optimizer(InferenceBase):
         return best_fit, logL_best_fit, extra_fields, runtime
 
 
-class Metrics(object):
-    def __init__(self, func):
+class MinimizeMetrics(object):
+    """simple callable class used as callback in scipy.optimize.minimize method"""
+    
+    def __init__(self, func, method):
         self.loss_history = []
         self.param_history = []
         self._func = func
+        if method == 'trust-constr':
+            self._call = self._call_2args
+        else:
+            self._call = self._call_1arg
+
+    def __call__(self, *args, **kwargs):
+        return self._call(*args, **kwargs)
         
-    def __call__(self, v):
-        self.loss_history.append(self._func(v))
-        self.param_history.append(v)
+    def _call_1arg(self, x):
+        self.loss_history.append(self._func(x))
+        self.param_history.append(x)
+
+    def _call_2args(self, x, state):
+        # Input state parameter is necessary for 'trust-constr' method
+        # You can use it to stop execution early by returning True
+        self.loss_history.append(self._func(x))
+        self.param_history.append(x)
