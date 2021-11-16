208553585921e7e9018983174c55266713be4e56
Aymeric Galan <aymeric.galan@gmail.com>
2021-11-16 23:41:32 +0100

    Add function to compute sensitivity maps

--- herculens/Util/model_util.py
+++ herculens/Util/model_util.py
@@ -1,12 +1,9 @@
 import copy
 import numpy as np
 import jax.numpy as jnp
+import jax
 from scipy.ndimage import morphology
 from scipy import ndimage
-
-from herculens.LightModel.light_model import LightModel
-from herculens.LensImage.lens_image import LensImage
-from herculens.Util import param_util
 
 
 def mask_from_source_area(lens_image, parameters):
@@ -29,6 +26,8 @@ def mask_from_source_area(lens_image, parameters):
 
 def mask_from_lensed_source(lens_image, parameters=None, source_model=None,
                             threshold=0.1, smoothing=0):
+    from herculens.LightModel.light_model import LightModel
+
     if parameters is None and source_model is None:
         raise ValueError("You must provide an the source model "
                          "if no parameters are provided.")
@@ -64,6 +63,8 @@ def mask_from_lensed_source(lens_image, parameters=None, source_model=None,
 
 def pixelated_region_from_sersic(kwargs_sersic, use_major_axis=False,
                                  min_width=1.0, min_height=1.0, scaling=1.0):
+    from herculens.Util import param_util
+
     # TODO: support arbitrary smooth source profile
     c_x = kwargs_sersic['center_x']
     c_y = kwargs_sersic['center_y']
@@ -90,37 +91,81 @@ def pixelated_region_from_sersic(kwargs_sersic, use_major_axis=False,
     return kwargs_pixelated_grid
 
 
-def R_omega(z, t, q, nmax):
-    """Angular dependency of the deflection angle in the EPL lens profile.
+def halo_sensitivity_map(macro_lens_image, macro_parameters, data, 
+                         halo_profile='SIS', kwargs_numerics=None,
+                         x_grid=None, y_grid=None):
+    # imports are here to avoid issues with circular imports
+    from herculens.LightModel.light_model import LightModel
+    from herculens.LensModel.lens_model import LensModel
+    from herculens.LensImage.lens_image import LensImage
+    from herculens.Parameters.parameters import Parameters
+    from herculens.Inference.loss import Loss
+    from herculens.Util import util
 
-    The computation follows Eqs. (22)-(29) in Tessore & Metcalf (2015), where
-    z = R * e^(i * phi) is a position vector in the lens plane,
-    t = gamma - 1 is the logarithmic slope of the profile, and
-    q is the axis ratio.
-
-    This iterative implementation is necessary, since the usual hypergeometric
-    function `hyp2f1` provided in `scipy.special` has not yet been implemented
-    in an autodiff way in JAX.
-
-    Note that the value returned includes an extra factor R multiplying Eq. (23)
-    for omega(phi).
-
-    """
-    # Set the maximum number of iterations
-    # nmax = 10
+    if halo_profile == 'SIS':
+        halo_profile_ = 'SIE'
+        kwargs_halo_fixed = {'e1': 0., 'e2': 0.}
+        kwargs_halo_init = {'theta_E': 0., 'center_x': 0., 'center_y': 0.}
+    else:
+        raise NotImplementedError(f"Halo profile '{halo_profile}' is not yet supported.")
     
-    # Compute constant factors
-    f = (1. - q) / (1. + q)
-    ei2phi = z / z.conjugate()
-    # Set the first term of the series
-    omega_i = z  # jnp.array(np.copy(z))  # Avoid overwriting z ?
-    partial_sum = omega_i
+    halo_lens_model_list = macro_lens_image.LensModel.lens_model_list + [halo_profile_]
+    halo_lens_model = LensModel(halo_lens_model_list)
 
-    for i in range(1, nmax):
-        # Iteration-dependent factor
-        ratio = (2. * i - (2. - t)) / (2. * i + (2 - t))
-        # Current Omega term proportional to the previous term
-        omega_i = -f * ratio * ei2phi * omega_i
-        # Update the partial sum
-        partial_sum += omega_i
-    return partial_sum
+    grid = copy.deepcopy(macro_lens_image.Grid)
+    grid.remove_model_grid('lens')
+    psf = copy.deepcopy(macro_lens_image.PSF)
+    noise = copy.deepcopy(macro_lens_image.Noise)
+    halo_lens_image = LensImage(grid, psf, noise_class=noise,
+                                lens_model_class=halo_lens_model,
+                                source_model_class=macro_lens_image.SourceModel,
+                                lens_light_model_class=macro_lens_image.LensLightModel,
+                                kwargs_numerics=kwargs_numerics)
+
+    kwargs_macro = macro_parameters.current_values(as_kwargs=True)
+    kwargs_fixed = {
+        'kwargs_lens': kwargs_macro['kwargs_lens'] + [kwargs_halo_fixed],
+        'kwargs_source': kwargs_macro['kwargs_source'],
+        'kwargs_lens_light': kwargs_macro['kwargs_lens_light'],
+    }
+    kwargs_init = {
+        'kwargs_lens': [{} for i in range(len(kwargs_macro['kwargs_lens']))] + [kwargs_halo_init],
+        'kwargs_source': [{} for i in range(len(kwargs_macro['kwargs_source']))],
+        'kwargs_lens_light': [{} for i in range(len(kwargs_macro['kwargs_lens_light']))],
+    }
+    halo_parameters = Parameters(halo_lens_image, kwargs_init, kwargs_fixed)
+
+    # create the loss to minimize
+    halo_loss = Loss(data, halo_lens_image, halo_parameters, likelihood_type='chi2')
+
+    # define the function that computes sensitivity at a given pixel (x, y)
+    @jax.jit
+    def sensitivity_at_pixel(x, y):
+        mass_proxy_param = 0.0  # typically this is theta_E 
+        grad_loss_mass = jax.grad(halo_loss)([mass_proxy_param, x, y])[0]
+        return grad_loss_mass
+
+    # efficiently compute sensitivity on the data grid
+    if halo_lens_image.ImageNumerics.grid_supersampling_factor > 1:
+        # note: those are 1D arrays
+        x_grid, y_grid = halo_lens_image.ImageNumerics.coordinates_evaluate
+    elif x_grid is None or y_grid is None:
+        x_grid, y_grid = halo_lens_image.Grid.pixel_coordinates
+    
+    # evaluate the sensitivity over the coordinates grid
+    sensitivity_map = jnp.vectorize(sensitivity_at_pixel)(x_grid, y_grid).block_until_ready()
+
+    # convert to numpy array and reshape
+    sensitivity_map = np.array(sensitivity_map)
+    if len(sensitivity_map.shape) == 1:
+        sensitivity_map = util.array2image(sensitivity_map)
+
+    # get the coordinates where the sensitivity is the highest
+    from skimage import feature
+    peak_indices_2d = feature.peak_local_max(-sensitivity_map)
+    
+    x_coords, y_coords = halo_lens_image.Grid.pixel_axes
+    x_coords_min = x_coords[peak_indices_2d[:, 1]]
+    y_coords_min = y_coords[peak_indices_2d[:, 0]]
+
+    return sensitivity_map, (x_coords_min, y_coords_min)
