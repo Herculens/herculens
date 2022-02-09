dbd688366af7be60fcdfba02a2a0b0d595aa0195
Egor Danilov <egor.danilov@epfl.ch>
2022-02-09 18:49:47 +0100

    New class for GRF inhomogeneities + its unit tests

---
+++ GRF_perturbations/Modules/GRF_inhomogeneities.py
@@ -0,0 +1,283 @@
+#Basic imports
+import numpy as np
+from copy import deepcopy
+import math
+
+#JAX
+import jax.numpy as jnp
+from jax.config import config
+config.update("jax_enable_x64", True)
+config.update("jax_debug_nans", True)
+
+def Box_Muller_transform():
+    # Polar Box-Muller transform samples independent stadard normal deviates
+    #Samples real and imaginary parts of Fourier image for unit power
+    s = 1.1
+    while s > 1. :
+        u = np.random.uniform (-1.,1.)
+        v = np.random.uniform (-1.,1.)
+        s = u**2. + v**2.
+    fac = np.sqrt(-2.*np.log(s)/s)
+    z1 = u*fac
+    z2 = v*fac
+    return z1,z2
+
+class GRF_inhomogeneities:
+
+    def __init__(self,pixel_number: int,pixel_scale: float,Phase_seeds_number: int):
+
+        self.pixel_number=pixel_number
+        self.pixel_scale=pixel_scale
+        self.Phase_seeds_number=Phase_seeds_number
+
+        #1d Spatial frequencies
+        k_vector = np.fft.fftfreq(pixel_number, pixel_scale)
+        #2d matrices for wavevector components
+        kx, ky = np.meshgrid(k_vector, k_vector)
+        #wavevector amplitudes
+        self.k_grid = np.sqrt(kx ** 2 + ky ** 2)
+        #Soften zero wavenumber, so spectrum doesn't diverge
+        self.nonsingular_k_grid=deepcopy(self.k_grid)
+        self.nonsingular_k_grid[0, 0] = 1
+
+        #Precompute Fourier images for unit spectrum, because jax doesn't tolerate while loops inside pure functions
+        self.tensor_unit_Fourier_images = np.zeros((self.Phase_seeds_number, self.pixel_number, self.pixel_number),dtype=complex)
+        for i in range(self.Phase_seeds_number):
+            self.tensor_unit_Fourier_images[i] = self.sample_unit_Fourier_image(random_seed=i)
+
+        # Return seed to the fixed one
+        np.random.seed(42)
+
+    def sample_unit_Fourier_image(self,random_seed):
+        """
+        Samples random Fourier image for unit power spectrum of GRF
+        Parameters
+        ----------
+        random_seed: int
+        seed for random Fourier phase generation
+
+        Returns
+        -------
+        unit_Fourier_image: np.ndarray complex (pixel_number,pixel_number)
+        Fourier image for unit power spectrum sampled using Polar Box-Muller transform
+        """
+        np.random.seed(random_seed)
+
+        Fourier_image = np.zeros([self.pixel_number, self.pixel_number], dtype='cfloat')  # Empty matrix to be filled in for the Fourier plane
+        j = 0 + 1j  # Defining the complex number
+
+        half_length=math.ceil(self.pixel_number/2.)
+
+        #Define top part of Fourier image and obtain bottom as complex conjugate
+        for y in range(half_length+1):
+            for x in range(self.pixel_number):
+
+                #Zero mean value of the field
+                if (y==0) and (x==0):
+                    Fourier_image[y,x]=0.
+
+                #Real and Imaginary parts
+                z1, z2 = Box_Muller_transform()
+
+                #Points where Fourier image should be real for configuration image to be real
+                if (np.isin(y,[0,half_length]) and np.isin(x,[0,half_length])):
+                    Fourier_image[y,x]=z1
+                else:
+                    Fourier_image[y,x]=(z1+j*z2)/np.sqrt(2)
+
+                #Complex conjugation
+                Fourier_image[-y, -x] = Fourier_image[y, x].conjugate()
+
+        return Fourier_image
+
+    def nonsingular_Power_spectrum(self,Spectrum_parameters):
+        """
+        Generates 2d matrix of power spectrum A*k^(-beta) for given [log(A),beta]
+        The spectrum is softened in k=0 to not diverge
+        Parameters
+        ----------
+        Spectrum_parameters: [float,float]
+            [log(A),Beta] as log(Amplitude) and minus Power slope of the power spectrum
+
+        Returns
+        -------
+        Power_spectrum: jnp.ndarray real (pixel_number,pixel_number)
+            Softened power spectrum for given A and Beta
+        """
+        logA,Beta=Spectrum_parameters
+
+        Amplitude = jnp.power(10., logA)
+        Power_spectrum = Amplitude * jnp.power(self.nonsingular_k_grid, -Beta)
+
+        return Power_spectrum
+
+    def field_variance(self,Spectrum_parameters,field='potential'):
+        """
+        Computes variance of a given field using Parseval's theorem
+        This will the theoretical variance, i.e. the field variance averaged over random seeds
+        Parameters
+        ----------
+        Spectrum_parameters: [float,float]
+            [log(A),Beta] as log(Amplitude) and minus Power slope of the power spectrum
+
+        field: str
+            Variance of which field to compute. Fields: 'potential','alpha_y','alpha_x','kappa'
+
+        Returns
+        -------
+        Variance: float
+            Variance of a given field for given power spectrum parameters
+        """
+
+        # Power spectrum softened in wavevector=0
+        potential_Power_spectrum = self.nonsingular_Power_spectrum(Spectrum_parameters)
+
+        # Zero mean mask
+        mask=np.ones_like(potential_Power_spectrum)
+        mask[0,0]=0
+        potential_Power_spectrum*=mask
+
+        if field=='potential':
+            return potential_Power_spectrum.sum()
+
+        # Derivative in Config space is multiplication by k in Fourier space
+        k_vector = np.fft.fftfreq(self.pixel_number, self.pixel_scale)
+        kx, ky = np.meshgrid(k_vector, k_vector)
+
+        if field=='alpha_y':
+            derivative_factor = (2 * np.pi * ky)**2  # d(psi)/dy
+        elif field=='alpha_x':
+            derivative_factor = (2 * np.pi * kx) ** 2  # d(psi)/dy
+        elif field=='kappa':
+            derivative_factor = (1 / 4) * (2 * np.pi) ** 4 * (kx ** 4 + ky ** 4) # laplacian(psi)/2
+        else:
+            raise ValueError("field should be one of: 'potential','alpha_y','alpha_x','kappa'")
+
+        return (potential_Power_spectrum*derivative_factor).sum()
+
+
+    def potential(self,Spectrum_parameters,unit_Fourier_image):
+        """
+        Function generating GRF gravitational potential
+        We have to pass Fourier image explicitly, to eventually compile the function
+        to a pure one: potential(Spectrum_parameters)
+        Parameters
+        ----------
+        Spectrum_parameters: [float,float]
+            [log(A),Beta] as log(Amplitude) and minus Power slope of the power spectrum
+
+        unit_Fourier_image: np.ndarray complex (pixel_number,pixel_number)
+            complex matrix obtained from self.sample_unit_Fourier_image
+
+        Returns
+        -------
+        potential: np.ndarray real (pixel_number,pixel_number)
+            Gravitational potential of GRF inhomogeneities
+        """
+
+        #Power spectrum softened in wavevector=0
+        Power_spectrum=self.nonsingular_Power_spectrum(Spectrum_parameters)
+
+        #Fourier image of GRF to generate
+        Fourier_image=jnp.sqrt(Power_spectrum)*unit_Fourier_image
+        #Conf image that needs proper norm
+        Configuration_image = jnp.fft.ifftshift(jnp.fft.ifftn(Fourier_image))
+
+        # Normalisation for Parseval's theorem (Power_spectrum.sum()=potential.var())
+        Normalisation_factor = self.pixel_number*self.pixel_number
+        potential = Normalisation_factor * Configuration_image.real
+
+        #Account for softening of Power spectrum by subtracting mean
+        zero_mean_potential=potential-potential.mean()
+
+        return zero_mean_potential
+
+    def alpha(self,Spectrum_parameters,unit_Fourier_image,direction='y'):
+        """
+        Differential deflection of GRF inhomogeneities for given direction
+        Parameters
+        ----------
+        Spectrum_parameters: [float,float]
+            [log(A),Beta] as log(Amplitude) and minus Power slope of the power spectrum
+
+        unit_Fourier_image: np.ndarray complex (pixel_number,pixel_number)
+            complex matrix obtained from self.sample_unit_Fourier_image
+
+        direction: 'y' or 'x'
+            deflection is derivative d(psi)/dy or d(psi)/dx correspondingly
+
+        Returns
+        -------
+        alpha: np.ndarray real (pixel_number,pixel_number)
+            differential deflection of GRF inhomogeneities for given direction
+        """
+
+        # Power spectrum softened in wavevector=0
+        potential_Power_spectrum = self.nonsingular_Power_spectrum(Spectrum_parameters)
+
+        #Derivative in Config space is multiplication by k in Fourier space
+        k_vector = np.fft.fftfreq(self.pixel_number, self.pixel_scale)
+        kx, ky = np.meshgrid(k_vector, k_vector)
+
+        if direction=='y':
+            derivative_factor=(2*np.pi*ky)**2 #d(psi)/dy
+        elif direction=='x':
+            derivative_factor=(2*np.pi*kx)**2 #d(psi)/dx
+        else:
+            raise ValueError("Derivative direction should be 'x' or 'y'")
+
+        #Power spectrum of differential deflection for a given direction
+        alpha_Power_spectrum=potential_Power_spectrum*derivative_factor
+
+        # Fourier image of GRF to generate
+        Fourier_image = jnp.sqrt(alpha_Power_spectrum) * unit_Fourier_image
+        # Conf image that needs proper norm
+        Configuration_image = jnp.fft.ifftshift(jnp.fft.ifftn(Fourier_image))
+
+        # Normalisation for Parseval's theorem (Power_spectrum.sum()=potential.var())
+        Normalisation_factor = self.pixel_number * self.pixel_number
+        alpha = Normalisation_factor * Configuration_image.real
+
+
+        return alpha
+
+    def kappa(self,Spectrum_parameters,unit_Fourier_image):
+        """
+        Differential convergence of GRF inhomogeneities
+        Parameters
+        ----------
+        Spectrum_parameters: [float,float]
+            [log(A),Beta] as log(Amplitude) and minus Power slope of the power spectrum
+
+        unit_Fourier_image: np.ndarray complex (pixel_number,pixel_number)
+            complex matrix obtain
+        Returns
+        -------
+        kappa: np.ndarray real (pixel_number,pixel_number)
+            differential convergence of GRF inhomogeneities
+        """
+
+        # Power spectrum softened in wavevector=0
+        potential_Power_spectrum = self.nonsingular_Power_spectrum(Spectrum_parameters)
+
+        # Derivative in Config space is multiplication by k in Fourier space
+        k_vector = np.fft.fftfreq(self.pixel_number, self.pixel_scale)
+        kx, ky = np.meshgrid(k_vector, k_vector)
+
+        #kappa is laplacian(psi)/2
+        derivative_factor=(1/4)*(2*np.pi)**4*(kx**4+ky**4)
+
+        # Power spectrum of differential convergence
+        kappa_Power_spectrum = potential_Power_spectrum * derivative_factor
+
+        # Fourier image of GRF to generate
+        Fourier_image = jnp.sqrt(kappa_Power_spectrum) * unit_Fourier_image
+        # Conf image that needs proper norm
+        Configuration_image = jnp.fft.ifftshift(jnp.fft.ifftn(Fourier_image))
+
+        # Normalisation for Parseval's theorem (Power_spectrum.sum()=potential.var())
+        Normalisation_factor = self.pixel_number * self.pixel_number
+        kappa = Normalisation_factor * Configuration_image.real
+
+        return kappa
+
---
+++ GRF_perturbations/Modules/tests/test_GRF_inhomogeneities.py
@@ -0,0 +1,112 @@
+import unittest
+import pytest
+
+from GRF_perturbations.Modules.GRF_inhomogeneities import *
+import scipy as sc
+
+
+class test_GRF_inhomogeneities(unittest.TestCase):
+
+    def setUp(self):
+        self.GRF_class = GRF_inhomogeneities(100, 0.08, 100)
+
+    def test_box_muller(self):
+        BM_variates = np.zeros((1000, 2))
+        for i in range(1000):
+            np.random.seed(i)
+            BM_variates[i] = Box_Muller_transform()
+
+        np.random.seed(42)
+
+        # Should be independent
+        p_value = sc.stats.spearmanr(BM_variates[:, 0], BM_variates[:, 1])[1]
+        self.assertGreater(p_value, 0.5)
+
+        self.assertAlmostEqual(BM_variates[:, 0].std(), 1., delta=0.1, msg='Std is not 1')
+        self.assertAlmostEqual(BM_variates[:, 1].std(), 1., delta=0.1, msg='Std is not 1')
+
+        self.assertAlmostEqual(BM_variates[:, 0].mean(), 0., delta=0.1, msg='Mean is not 0')
+        self.assertAlmostEqual(BM_variates[:, 1].mean(), 0., delta=0.1, msg='Mean is not 0')
+
+
+    def test_sample_unit_fourier_image(self):
+        # Consider only uncorrected points
+        mask_full_variates = np.ones((100, 100), dtype=bool)
+        mask_full_variates[0, 0] = False
+        mask_full_variates[0, 50] = False
+        mask_full_variates[50, 0] = False
+        mask_full_variates[50, 50] = False
+
+        # Real and Image should be standard normal
+        Fourier_values = (np.sqrt(2)) * self.GRF_class.tensor_unit_Fourier_images[:, mask_full_variates]
+        self.assertTrue(np.allclose(Fourier_values.real.std(axis=1), 1., rtol=0.05), msg='Std is not 1')
+        self.assertTrue(np.allclose(Fourier_values.imag.std(axis=1), 1., rtol=0.05), msg='Std is not 1')
+
+    def test_potential(self):
+
+        logA_array=[-9.,-8.,-7.]
+        Beta_array=[0,2,4]
+
+        theoretical_Variances=np.zeros((len(logA_array),len(Beta_array),100))
+        generated_Variances=np.zeros((len(logA_array),len(Beta_array),100))
+
+        for i,logA in enumerate(logA_array):
+            for j,Beta in enumerate(Beta_array):
+                #100 variances for each logA,Beta from Parseval's theorem
+                theoretical_Variances[i,j]=np.repeat(self.GRF_class.field_variance([logA,Beta],field='potential'),100)
+                #100 variances for each logA,Beta from actually sampled field
+                Potentials = np.array([self.GRF_class.potential([logA,Beta],self.GRF_class.tensor_unit_Fourier_images[seed])\
+                                       for seed in range(100)])
+                generated_Variances[i,j]=Potentials.var(axis=(-1,-2))
+
+        #Test that logVariances match
+        self.assertTrue(np.allclose(np.log(generated_Variances),np.log(theoretical_Variances),rtol=0.25))
+
+    def test_alpha(self):
+
+        logA_array = [-9., -8., -7.]
+        Beta_array = [0, 2, 4]
+
+        theoretical_Variances = np.zeros((len(logA_array), len(Beta_array), 100, 2))
+        generated_Variances = np.zeros((len(logA_array), len(Beta_array), 100, 2))
+
+        for i, logA in enumerate(logA_array):
+            for j, Beta in enumerate(Beta_array):
+                # 100 variances for each logA,Beta from Parseval's theorem
+                theoretical_Variances[i, j,:,0] = np.repeat(self.GRF_class.field_variance([logA, Beta], field='alpha_y'),100)
+                theoretical_Variances[i, j,:,1] = np.repeat(self.GRF_class.field_variance([logA, Beta], field='alpha_x'),100)
+
+                # 100 variances for each logA,Beta from actually sampled field
+                alphas_y = np.array([self.GRF_class.alpha([logA, Beta], self.GRF_class.tensor_unit_Fourier_images[seed],\
+                                                        direction='y') for seed in range(100)])
+                alphas_x = np.array([self.GRF_class.alpha([logA, Beta], self.GRF_class.tensor_unit_Fourier_images[seed], \
+                                                          direction='x') for seed in range(100)])
+
+                generated_Variances[i, j,:,0] = alphas_y.var(axis=(-1, -2))
+                generated_Variances[i, j, :, 1] = alphas_x.var(axis=(-1, -2))
+
+        # Test that logVariances match
+        self.assertTrue(np.allclose(np.log(generated_Variances), np.log(theoretical_Variances), rtol=0.25))
+
+    def test_kappa(self):
+
+        logA_array=[-9.,-8.,-7.]
+        Beta_array=[0,2,4]
+
+        theoretical_Variances=np.zeros((len(logA_array),len(Beta_array),100))
+        generated_Variances=np.zeros((len(logA_array),len(Beta_array),100))
+
+        for i,logA in enumerate(logA_array):
+            for j,Beta in enumerate(Beta_array):
+                #100 variances for each logA,Beta from Parseval's theorem
+                theoretical_Variances[i,j]=np.repeat(self.GRF_class.field_variance([logA,Beta],field='kappa'),100)
+                #100 variances for each logA,Beta from actually sampled field
+                kappas = np.array([self.GRF_class.kappa([logA,Beta],self.GRF_class.tensor_unit_Fourier_images[seed])\
+                                       for seed in range(100)])
+                generated_Variances[i,j]=kappas.var(axis=(-1,-2))
+
+        # Test that logVariances match
+        self.assertTrue(np.allclose(np.log(generated_Variances), np.log(theoretical_Variances), rtol=0.25))
+
+if __name__ == '__main__':
+    unittest.main()
