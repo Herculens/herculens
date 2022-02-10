d0cfd350a579dd3a6cb5c607731a926f2315f88c
Aymeric Galan <aymeric.galan@gmail.com>
2022-02-10 16:01:36 +0100

    Improvements to the interface of FisherCovariance class

--- herculens/Parameters/covariance.py
+++ herculens/Parameters/covariance.py
@@ -10,8 +10,37 @@ class FisherCovariance(object):
         self.inference = inference_class
 
     @property
-    def full_covar_matrix(self):
-        return np.linalg.inv(self._fim)
+    def fisher_matrix(self):
+        if not hasattr(self, '_fim'):
+            raise ValueError("Call first compute_fisher_information().")
+        return self._fim
+
+    @property
+    def covariance_matrix(self):
+        if not hasattr(self, '_cov'):
+            self._cov = self.fisher2covar(self.fisher_matrix, inversion='full')
+        return self._cov
+
+    def draw_samples(self, num_samples=10000, seed=None):
+        if seed is not None:
+            np.random.seed(seed)
+        mean = self.param.current_values()
+        cov  = self.covariance_matrix
+        samples = np.random.multivariate_normal(mean, cov, size=num_samples)
+        return samples
+
+    def get_kwargs_sigma(self):
+        sigma_values = np.sqrt(np.abs(np.diag(self.covariance_matrix)))
+        return self.param.args2kwargs(sigma_values)
+
+    def compute_fisher_information(self, recompute=False):
+        if hasattr(self, '_fim') and not recompute:
+            return  # nothing to do
+        best_fit_values = self.param.current_values()
+        self._fim = self.inference.hessian(best_fit_values).block_until_ready()
+        self._fim = np.array(self._fim)
+        if hasattr(self, '_cov'):
+            delattr(self, '_cov')
 
     def fisher2covar(self, fisher_matrix, inversion='full'):
         if inversion == 'full':
@@ -20,18 +49,6 @@ class FisherCovariance(object):
             return 1. / np.diag(fisher_matrix)
         else:
             raise ValueError("Only 'full' and 'diag' options are supported for inverting the FIM.")
-
-    @property
-    def fisher_matrix(self):
-        if not hasattr(self, '_fim'):
-            raise ValueError("The FIM has not been computed.")
-        return self._fim
-
-    def compute_fisher_information(self, best_fit_values, recompute=False):
-        if not recompute and hasattr(self, '_fim'):
-            pass
-        self._fim = self.inference.hessian(best_fit_values).block_until_ready()
-        self._fim = np.array(self._fim)
 
     def split_fisher_matrix(self, num_before, num_after):
         fim_interior = self.fisher_matrix[num_before:-num_after, num_before:-num_after]
