57b146094b05b72aa8f6d3e53421790efeb3151c
Aymeric Galan <aymeric.galan@gmail.com>
2022-03-08 17:15:25 +0100

    Implement sensitivity mapping with re-optimization of parameters

--- herculens/Inference/sensitivity_mapping.py
+++ herculens/Inference/sensitivity_mapping.py
@@ -15,6 +15,8 @@ from herculens.LensModel.lens_model import LensModel
 from herculens.LensImage.lens_image import LensImage
 from herculens.Parameters.parameters import Parameters
 from herculens.Inference.loss import Loss
+from herculens.Inference.optimization import Optimizer
+# from herculens.Parameters.covariance import FisherCovariance
 from herculens.Util import util
 
 __all__ = ['MassSensitivityMapping']
@@ -42,7 +44,8 @@ class MassSensitivityMapping(object):
         self.halo_profile = halo_profile
         self.verbose = verbose
 
-    def sensitivity_map(self, init_mass=0., x_grid=None, y_grid=None, use_jax_vectorize=False):
+    def sensitivity_map(self, init_mass=0., x_grid=None, y_grid=None, 
+                        use_jax_vectorize=False):
         # prepare the new model
         self.prepare_halo_model(init_mass)
 
@@ -93,9 +96,88 @@ class MassSensitivityMapping(object):
         x_coords, y_coords = x_grid[0, :], y_grid[:, 0]
         x_minima = x_coords[peak_indices_2d[:, 1]]
         y_minima = y_coords[peak_indices_2d[:, 0]]
-        z_minima = sensitivity_map[peak_indices_2d[:, 1], peak_indices_2d[:, 0]]
+        z_minima = sensitivity_map[peak_indices_2d[:, 0], peak_indices_2d[:, 1]]
+
+        # if with_covariance:
+        #     @jit
+        #     def covariance_at_pixel(x, y):
+        #         if self.fix_macro:
+        #             p = [init_mass, x, y]
+        #         else:
+        #             p = [init_mass, x, y] + self.p_macro
+        #         grad_loss_mass = self.halo_loss.hessian(p)
+        #         partial_deriv_mass = grad_loss_mass[0]
+        #         return partial_deriv_mass
+        #     covariance_cube = np.vectorize(covariance_at_pixel)(x_grid, y_grid)
 
         return sensitivity_map, (x_minima, y_minima, z_minima), runtime
+
+    def sensitivity_map_optim(self, init_mass=0., x_grid=None, y_grid=None, **kwargs_optimizer):
+        # prepare the new model
+        self.prepare_halo_model(init_mass)
+
+        # create the loss corresponding this new model
+        self.halo_loss = Loss(self.data, self.halo_lens_image, self.halo_param, likelihood_type='chi2')
+
+        # create the optimizer
+        optimizer = Optimizer(self.halo_loss, self.halo_param)
+
+        # define the grid on which to compute sensitivity
+        if self.halo_lens_image.ImageNumerics.grid_supersampling_factor > 1:
+            x_grid, y_grid = self.halo_lens_image.ImageNumerics.coordinates_evaluate # 1D arrays
+        elif x_grid is None or y_grid is None:
+            x_grid, y_grid = self.halo_lens_image.Grid.pixel_coordinates
+
+        # CHECKS
+        if self.verbose:
+            print("halo loss at edge:", self.halo_loss([init_mass, x_grid[0, 0], y_grid[0, 0]]))
+            print("halo loss on arc:", self.halo_loss([init_mass, 1.1, 1.1]))
+            print("halo grad loss on arc:", self.halo_loss.gradient([init_mass, 1.1, 1.1]))
+
+            print("macro loss:", self.m_loss(self.p_macro))
+            # print("macro grad loss:", grad(self.m_loss)(self.p_macro))
+
+        def sensitivity_at_pixel(x, y):
+            if self.fix_macro:
+                p = [init_mass, x, y]
+            else:
+                p = [init_mass, x, y] + self.p_macro
+            best_fit, logL, _, runtime = optimizer.minimize('trust-krylov', init_params=p, **kwargs_optimizer)
+            return float(best_fit[0]), logL, runtime
+        
+        # evaluate the sensitivity over the coordinates grid
+        start = time.time()
+        sensitivity_map, logL_map, runtimes = np.vectorize(sensitivity_at_pixel)(x_grid, y_grid)
+        runtime = np.sum(runtimes)
+
+        # convert to numpy array and reshape
+        sensitivity_map = np.array(sensitivity_map)
+        logL_map = np.array(logL_map)
+        if len(sensitivity_map.shape) == 1:
+            sensitivity_map = util.array2image(sensitivity_map)
+            logL_map = util.array2image(logL_map)
+
+        # get the coordinates where the sensitivity is the highest
+        peak_indices_2d = feature.peak_local_max(-np.clip(sensitivity_map, a_min=None, a_max=0))
+        
+        x_coords, y_coords = x_grid[0, :], y_grid[:, 0]
+        x_minima = x_coords[peak_indices_2d[:, 1]]
+        y_minima = y_coords[peak_indices_2d[:, 0]]
+        z_minima = sensitivity_map[peak_indices_2d[:, 0], peak_indices_2d[:, 1]]
+
+        # if with_covariance:
+        #     @jit
+        #     def covariance_at_pixel(x, y):
+        #         if self.fix_macro:
+        #             p = [init_mass, x, y]
+        #         else:
+        #             p = [init_mass, x, y] + self.p_macro
+        #         grad_loss_mass = self.halo_loss.hessian(p)
+        #         partial_deriv_mass = grad_loss_mass[0]
+        #         return partial_deriv_mass
+        #     covariance_cube = np.vectorize(covariance_at_pixel)(x_grid, y_grid)
+
+        return sensitivity_map, (x_minima, y_minima, z_minima), logL_map, runtime
 
     def prepare_halo_model(self, init_mass):
         # value at which the gradient will be evaluated
