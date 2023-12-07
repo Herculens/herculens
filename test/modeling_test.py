01185798b9653e42211b0bb8ae0ddacca11cf5c7
Aymeric Galan <aymeric.galan@gmail.com>
2023-12-07 17:41:24 +0100

    Fix failing test due to change in PSF interface

--- test/modeling_test.py
+++ test/modeling_test.py
@@ -30,7 +30,6 @@ from herculens.LensImage.lens_image import LensImage
 from herculens.Inference.loss import Loss
 from herculens.Inference.ProbModel.numpyro import NumpyroModel
 from herculens.Inference.Optimization.jaxopt import JaxoptOptimizer
-from herculens.Util import image_util, param_util, plot_util
 
 
 def _simulate_data(data_type, supersampling_factor):
@@ -42,7 +41,7 @@ def _simulate_data(data_type, supersampling_factor):
     pixel_grid = PixelGrid(nx=npix, ny=npix, 
                            ra_at_xy_0=ra_at_xy_0, dec_at_xy_0=dec_at_xy_0,
                            transform_pix2angle=transform_pix2angle)
-    psf = PSF(psf_type='GAUSSIAN', fwhm=0.3)
+    psf = PSF(psf_type='GAUSSIAN', fwhm=0.3, pixel_size=pix_scl)
     noise = Noise(npix, npix, background_rms=1e-2, exposure_time=2000.)
 
     # Define simulation parameters
