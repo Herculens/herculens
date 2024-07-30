a851b6aa4f92b92cd67a5ecf45da92cf4eab8bb6
Aymeric Galan <aymeric.galan@gmail.com>
2024-07-30 12:57:53 +0100

    Release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.1.0 (2024-07-30)
+++++++++++++++++++
+
+* First release on PyPI.
--- herculens/__init__.py
+++ herculens/__init__.py
@@ -6,14 +6,14 @@ and provides some basic information about the package.
 """
 
 # Set the package release version
-version_info = (0, 0, 1)
+version_info = (0, 1, 0)
 __version__ = '.'.join(str(c) for c in version_info)
 
 # Set the package details
 __author__ = 'Aymeric Galan, Austin Peel, Giorgos Vernardos & Herculens contributors'
-__email__ = 'austin.peel@epfl.ch'
+__email__ = 'aymeric.galan@gmail.com'
 __year__ = '2021-2024'
 __credits__ = 'Herculens contributors, EPFL (see AUTHORS.md)'
-__url__ = 'https://github.com/austinpeel/herculens'
+__url__ = 'https://github.com/Herculens/herculens'
 __description__ = 'Auto-differentiable strong lens modelling'
 __license__ = 'MIT'
