46ffbe4629e5a21b10f5d3edea288158284a3297
Aymeric Galan <aymeric.galan@gmail.com>
2024-07-30 08:48:49 +0100

    Update setup.py

--- herculens/__init__.py
+++ herculens/__init__.py
@@ -1,4 +1,19 @@
-__author__ = 'Austin Peel, Aymeric Galan, Giorgos Vernardos and contributors'
+"""
+
+This file initializes the coolest module
+and provides some basic information about the package.
+
+"""
+
+# Set the package release version
+version_info = (0, 0, 1)
+__version__ = '.'.join(str(c) for c in version_info)
+
+# Set the package details
+__author__ = 'Aymeric Galan, Austin Peel, Giorgos Vernardos & Herculens contributors'
 __email__ = 'austin.peel@epfl.ch'
-__version__ = '0.0.1'
-__credits__ = 'EPFL'
+__year__ = '2021-2024'
+__credits__ = 'Herculens contributors, EPFL (see AUTHORS.md)'
+__url__ = 'https://github.com/austinpeel/herculens'
+__description__ = 'Auto-differentiable strong lens modelling'
+__license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -1,31 +1,66 @@
 import setuptools
 import os
 
-__name__ = 'herculens'
+name = 'herculens'
 
 release_info = {}
 infopath = os.path.abspath(os.path.join(os.path.dirname(__file__),
-                           __name__, 'info.py'))
+                           name, '__init__.py'))
 with open(infopath) as open_file:
     exec(open_file.read(), release_info)
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+    readme = f.read()
+
+# Use the README as long description but remove the second version 
+# of the logo (readable only by GitHub's dark mode interface)
+long_description = readme.replace('<img src="https://raw.githubusercontent.com/Herculens/herculens/main/images/horizontal_dark_bg.png#gh-dark-mode-only" width="600" alt="Herculens logo" />', '')
+
+# Python version
+python_requires = '>=3.8'
+
+# Minimal required packages (see requirements.txt for versions)
+install_requires = [
+    'numpy',
+    'scipy',
+    'jax',
+    'jaxlib',
+    'scikit-image',
+    'https://github.com/aymgal/utax.git',
+]
+
+# Minimal optional packages (see requirements.txt for versions and complete list)
+install_optional = [
+    'matplotlib',    # for plotting
+    'optax',         # for optimizers
+    'jaxopt',        # for optimizers
+    'findiff',       # for building matrix with finite difference coefficients
+    'numpyro',       # for SVI and HMC sampling
+    'blackjax',      # for JAX-based HMC sampling
+]
+
+version = release_info['__version__']
 
 setuptools.setup(
-    name=__name__,
+    name=name,
     author=release_info['__author__'],
     author_email=release_info['__email__'],
-    version=release_info['__version__'],
+    version=version,
     url=release_info['__url__'],
+    download_url=f"https://github.com/Herculens/herculens/archive/refs/tags/v{version}.tar.gz",
     packages=setuptools.find_packages(),
-    python_requires=release_info['__python__'],
-    install_requires=release_info['__requires__'],
     license=release_info['__license__'],
-    description=release_info['__about__'],
+    description=release_info['__description__'],
     long_description=long_description,
     long_description_content_type='text/markdown',
-    setup_requires=release_info['__setup_requires__'],
-    tests_require=release_info['__tests_require__']
+    keywords=["herculens", "lensing", "gravitation", "astrophysics"],
+
+    python_requires=python_requires,
+    install_requires=install_requires,
+    extras_require={
+        "opt": install_optional,  # installable via `pip install herculens[opt]`
+    },
+    setup_requires=['pytest-runner',],
+    tests_require=['pytest', 'pytest-cov', 'pytest-pep8'],
 )
