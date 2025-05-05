1bde65e49f9c5af82d141d1e8638e47c938b44e1
Aymeric Galan <aymeric.galan@gmail.com>
2025-05-05 12:25:14 +0200

    Minor changes to private interface of profiles

--- herculens/LightModel/Profiles/shapelets.py
+++ herculens/LightModel/Profiles/shapelets.py
@@ -12,19 +12,19 @@ __all__= ['Shapelets']
 
 class Shapelets(object):
     """Surface brightness with a set of basis functions"""
+
     param_names = ['beta', 'center_x', 'center_y', 'amps']
     lower_limit_default = {'beta': 0.,  'center_x': 100., 'center_y': -100., 'amps': -1e10}
     upper_limit_default = {'beta': 1e5, 'center_x': 100., 'center_y':  100., 'amps':  1e10}
     fixed_default = {key: False for key in param_names}
 
     def __init__(self, n_max, function_type='gaussian'):
-        from gigalens.jax.profiles.light.shapelets import Shapelets as ShapeletsGigaLens
-        
         if function_type == 'gaussian':
+            from gigalens.jax.profiles.light.shapelets import Shapelets as ShapeletsGigaLens
             self._n_max = n_max
             self._backend = ShapeletsGigaLens(self._n_max, use_lstsq=False, interpolate=False)
         else:
-            raise ValueError(f"Basis function type '{function_type}' is not supported")
+            raise NotImplementedError(f"Basis function type '{function_type}' is not supported")
         self._func_type = function_type
 
     @property
--- herculens/MassModel/Profiles/piemd.py
+++ herculens/MassModel/Profiles/piemd.py
@@ -17,47 +17,62 @@ __author__ = 'HanWang2021', 'aymgal'
 
 
 from functools import partial
+import numpy as np
 import jax
 import jax.numpy as jnp
 from jax.tree_util import register_pytree_node
 
 
+
+def rotation(xx, yy, phi):
+    cos_phi, sin_phi = jnp.cos(phi), jnp.sin(phi)
+    xx_rot = cos_phi*xx + sin_phi*yy
+    yy_rot = -sin_phi*xx + cos_phi*yy
+    return xx_rot, yy_rot
+
+
 class PIEMD(object):
+    """
+    Pseudo isothermal elliptical mass distribution (PIEMD), based on the
+    implementation in the GLEE modelling code.
+    
+    The original GLEE parametrization is
+
+    kappa(x,y) = E0 / (2*Sqrt(w^2 + rem^2)),
+    where rem^2 = x^2/(1+e)^2 + y^2/(1-e)^2,
+
+    with parameters:
+    E0 is the lens strength,
+    w is the core radius, and
+    e is the ellipticity, e=(1.-q)/(1.+q)   [inverting gives q = (1-e)/(1+e)]
+
+    In the limiting case where w=e=0, E0 is the Einstein radius of the SIS.
+
+    If `scale_flag=False`, then E0 = thetaE
+
+    If `scale_flag=True`, then E0 = thetaE, E0 = E0*E0 / (sqrt(E0 * E0 + w * w) - w)
+
+    If (w < r_soft) then w will be set to `r_soft`.
+
+    In the Herculens-adapted code below, we use theta_E parameter for E0 (or Elim), phi for the position angle, and q for the axis ratio, 
+    and r_core for the core radius w.
+    The 'scale' flag and softening radius are set at construction time.
+
+    Parameters
+    ----------
+    r_soft : _type_, optional
+        Softening radius, by default 1e-8
+    scale_flag : bool, optional
+        'scale' flag usually given in GLEE config files, by default True, 
+        which leads to theta_E being simply the lensing strength.
+    """
+
+    param_names = ['theta_E', 'r_core', 'q', 'phi', 'center_x', 'center_y']
+    lower_limit_default = {'theta_E': 0, 'r_core': 0, 'q': 0.2, 'phi': -np.pi/2., 'center_x': -100, 'center_y': -100}
+    upper_limit_default = {'theta_E': 10, 'r_core': 1e10, 'q': 1., 'phi': +np.pi/2., 'center_x': 100, 'center_y': 100}
+    fixed_default = {key: False for key in param_names}
+
     def __init__(self, r_soft=1e-8, scale_flag=True):
-        """
-        Pseudo isothermal elliptical mass distribution (PIEMD), based on the
-        implementation in the GLEE modelling code.
-        
-        The original GLEE parametrization is
-
-        kappa(x,y) = E0 / (2*Sqrt(w^2 + rem^2)),
-        where rem^2 = x^2/(1+e)^2 + y^2/(1-e)^2,
-
-        with parameters:
-        E0 is the lens strength,
-        w is the core radius, and
-        e is the ellipticity, e=(1.-q)/(1.+q)   [inverting gives q = (1-e)/(1+e)]
-
-        In the limiting case where w=e=0, E0 is the Einstein radius of the SIS.
-
-        If `scale_flag=False`, then E0 = thetaE
-
-        If `scale_flag=True`, then E0 = thetaE, E0 = E0*E0 / (sqrt(E0 * E0 + w * w) - w)
-
-        If (w < r_soft) then w will be set to `r_soft`.
-
-        In the Herculens code below, we use theta_E parameter for E0 (or Elim), phi for the position angle, and q for the axis ratio, 
-        and r_core for the core radius w.
-        The 'scale' flag and softening radius are set at construction time.
-
-        Parameters
-        ----------
-        r_soft : _type_, optional
-            Softening radius, by default 1e-8
-        scale_flag : bool, optional
-            'scale' flag usually given in GLEE config files, by default True, 
-            which leads to theta_E being simply the lensing strength.
-        """
         self._r_soft = r_soft
         self._scale_flag = scale_flag
     
@@ -67,7 +82,12 @@ class PIEMD(object):
         e = (1.-q)/(1.+q)
         w = r_core
         theta_E = jnp.where(self._scale_flag, theta_E**2/(jnp.sqrt(theta_E*theta_E + w * w) - w), theta_E)
-        return jax.lax.cond(e != 0, self._poten_q, self._poten_spherical, x, y, center_x, center_y, e, phi, theta_E, w)
+        return jax.lax.cond(
+            e != 0, 
+            self._potential_elliptical, 
+            self._potential_spherical, 
+            x, y, center_x, center_y, e, phi, theta_E, w
+        )
     
     @partial(jax.jit, static_argnums=(0,))
     def derivatives(self, x, y, theta_E, r_core, q, phi, center_x=0, center_y=0):
@@ -75,20 +95,30 @@ class PIEMD(object):
         e = (1.-q)/(1.+q)
         w = r_core
         theta_E = jnp.where(self._scale_flag, theta_E**2/(jnp.sqrt(theta_E*theta_E + w * w) - w), theta_E)
-        return jax.lax.cond(e!=0, self._def_q, self._def_spherical, x, y, center_x, center_y, e, phi, theta_E, w)
+        return jax.lax.cond(
+            e!=0, 
+            self._deflection_elliptical, 
+            self._deflection_spherical, 
+            x, y, center_x, center_y, e, phi, theta_E, w
+        )
     
     @partial(jax.jit, static_argnums=(0,))
     def hessian(self, x, y, theta_E, r_core, q, phi, center_x=0, center_y=0):
         e = (1.-q)/(1.+q)
         w = r_core
         theta_E = jnp.where(self._scale_flag, theta_E**2/(jnp.sqrt(theta_E*theta_E + w * w) - w), theta_E)
-        return jax.lax.cond(e!=0, self._hessian_q, self._hessian_spherical, x, y, center_x, center_y, e, phi, theta_E, w)
+        return jax.lax.cond(
+            e!=0, 
+            self._hessian_elliptical, 
+            self._hessian_spherical,
+            x, y, center_x, center_y, e, phi, theta_E, w
+        )
     
     @partial(jax.jit, static_argnums=(0,))
     def kappa(self, x, y, theta_E, r_core, q, phi, center_x=0, center_y=0):
         xx = x - center_x
         yy = y - center_y
-        xx, yy = self._rotation(xx, yy, phi)
+        xx, yy = rotation(xx, yy, phi)
         r2 = xx**2 + yy**2/(q*q)
         w = r_core
         theta_E = jnp.where(self._scale_flag, theta_E**2/(jnp.sqrt(theta_E*theta_E + w * w) - w), theta_E)
@@ -96,11 +126,21 @@ class PIEMD(object):
         return kap
     
     @partial(jax.jit, static_argnums=(0,))
-    def _poten_q(self, x, y, x_centre, y_centre, e, pa, theta_E, w):
+    def _potential_spherical(self, x, y, x_centre, y_centre, _, pa, theta_E, w):
+        # potential, given q = 1
+        xx = x - x_centre
+        yy = y - y_centre
+        xx, yy = rotation(xx, yy, pa)
+        rm = jnp.sqrt((xx)**2 + (yy)**2)
+        phi = theta_E *(jnp.sqrt(rm*rm+w*w) - w*jnp.log(w+jnp.sqrt(rm*rm+w*w)) - w + w*jnp.log(2.*w) )
+        return phi
+    
+    @partial(jax.jit, static_argnums=(0,))
+    def _potential_elliptical(self, x, y, x_centre, y_centre, e, pa, theta_E, w):
         # potential, given q != 1
         xx = x - x_centre
         yy = y - y_centre
-        xx, yy = self._rotation(xx, yy, pa)
+        xx, yy = rotation(xx, yy, pa)
         #jax.debug.print("theta_E: {}", theta_E)
         rem = jnp.sqrt(xx*xx/((1+e)*(1+e))+yy*yy/((1-e)*(1-e)))
         rm = jnp.sqrt(xx*xx + yy*yy)
@@ -115,43 +155,33 @@ class PIEMD(object):
         phi = (theta_E*w*(1.-e*e)/(2.*rem*jnp.sqrt(e))) * jnp.imag((xx - 1j*yy) * Kstar)
         return phi
 
-    @partial(jax.jit, static_argnums=(0,))
-    def _poten_spherical(self, x, y, x_centre, y_centre, _, pa, theta_E, w):
-        # potential, given q = 1
-        xx = x - x_centre
-        yy = y - y_centre
-        xx, yy = self._rotation(xx, yy, pa)
-        rm = jnp.sqrt((xx)**2 + (yy)**2)
-        phi = theta_E *(jnp.sqrt(rm*rm+w*w) - w*jnp.log(w+jnp.sqrt(rm*rm+w*w)) - w + w*jnp.log(2.*w) )
-        return phi
-
-    def _def_q(self, x, y, x_centre, y_centre, e, pa, E0, w):
-        # return deflection_angle, given q!=1
-        dx =  x - x_centre
-        dy =  y - y_centre
-        dx, dy = self._rotation(dx, dy, pa)
-        Istar = ((-0.5j) * (1 - (e * e)) * E0 * jnp.log((((1 - e) * dx) / (1 + e) - (1j * (1 + e) * dy) / (1 - e) + (2j) * jnp.sqrt(e) * jnp.sqrt(w * w + (dx * dx) / ((1 + e) * (1 + e)) + (dy * dy) / ((1 - e) * (1 - e)))) / ((2j) * jnp.sqrt(e) * w + dx - (1j) * dy))) / jnp.sqrt(e)
-        alp_x, alp_y = jnp.real(Istar), jnp.imag(Istar)
-        alp_x, alp_y = self._rotation(alp_x, alp_y, -pa)
-        return alp_x, alp_y
-
-    def _def_spherical(self, x, y, x_centre, y_centre, e, pa, theta_E, w):
+    def _deflection_spherical(self, x, y, x_centre, y_centre, e, pa, theta_E, w):
         # return deflection_angle, q = 1, e is not used here, passing it as input is only because of the syntax of jax.lax.cond()
         tol = 1.e-10
         xx =  x - x_centre
         yy =  y - y_centre
-        xx, yy = self._rotation(xx, yy, pa)
+        xx, yy = rotation(xx, yy, pa)
         commen_factor = jnp.sqrt(w*w + xx*xx + yy*yy)/(xx*xx+yy*yy)
         condition  = (xx*xx+yy*yy)/(w*w)
         alp_x = jnp.where(condition>=tol, theta_E*xx*((-w/(xx*xx+ yy*yy) + commen_factor)), 0.5*theta_E*(xx)/w)
         alp_y = jnp.where(condition>=tol, theta_E*yy*(-(w/(xx*xx + yy*yy))+ commen_factor),  0.5*theta_E*(yy)/w)
-        alp_x, alp_y = self._rotation(alp_x, alp_y, -pa)
+        alp_x, alp_y = rotation(alp_x, alp_y, -pa)
+        return alp_x, alp_y
+
+    def _deflection_elliptical(self, x, y, x_centre, y_centre, e, pa, E0, w):
+        # return deflection_angle, given q!=1
+        dx =  x - x_centre
+        dy =  y - y_centre
+        dx, dy = rotation(dx, dy, pa)
+        Istar = ((-0.5j) * (1 - (e * e)) * E0 * jnp.log((((1 - e) * dx) / (1 + e) - (1j * (1 + e) * dy) / (1 - e) + (2j) * jnp.sqrt(e) * jnp.sqrt(w * w + (dx * dx) / ((1 + e) * (1 + e)) + (dy * dy) / ((1 - e) * (1 - e)))) / ((2j) * jnp.sqrt(e) * w + dx - (1j) * dy))) / jnp.sqrt(e)
+        alp_x, alp_y = jnp.real(Istar), jnp.imag(Istar)
+        alp_x, alp_y = rotation(alp_x, alp_y, -pa)
         return alp_x, alp_y
     
     def _hessian_spherical(self, x, y, x_centre, y_centre, e, pa ,theta_E, w):
         xx =  x - x_centre
         yy =  y - y_centre
-        dx, dy = self._rotation(xx, yy, pa)
+        dx, dy = rotation(xx, yy, pa)
         psi11 = (theta_E*(w*w*(-(dx)*(dx) + (dy)*(dy)) +(dy)*(dy)*((dx)*(dx) + (dy)*(dy)) +
 		          w*((dx) - (dy))*((dx) + (dy))*jnp.sqrt(w*w + (dx)*(dx) + (dy)*(dy))))/(jnp.power((dx)*(dx) + (dy)*(dy),2)*jnp.sqrt(w*w + (dx)*(dx) + (dy)*(dy)))
 
@@ -159,10 +189,10 @@ class PIEMD(object):
         psi12 = -((theta_E*(dx)*(dy)*((dx)*(dx) + (dy)*(dy) + 2*w*(w - jnp.sqrt(w*w + (dx)*(dx) + (dy)*(dy)))))/(jnp.power((dx)*(dx) + (dy)*(dy),2)*jnp.sqrt(w*w + (dx)*(dx) + (dy)*(dy))))
         return psi11, psi22, psi12
 
-    def _hessian_q(self, x, y, x_centre, y_centre, e, pa ,theta_E, w):
+    def _hessian_elliptical(self, x, y, x_centre, y_centre, e, pa ,theta_E, w):
         xx =  x - x_centre
         yy =  y - y_centre
-        dx, dy = self._rotation(xx, yy, pa)
+        dx, dy = rotation(xx, yy, pa)
 
         psi11= jnp.imag(((1 - (e*e))*theta_E*((2.j)*jnp.sqrt(e)*w + (dx) - (1.j)*(dy))*
 		            (((1 - e)/(1 + e) + ((2.j)*jnp.sqrt(e)*(dx))/
@@ -211,12 +241,6 @@ class PIEMD(object):
 	                       2.j*jnp.sqrt(e)*jnp.sqrt(w*w + (dx)*(dx)/((1+e)*(1+e)) +
 	                        (dy)*(dy)/((1-e)*(1-e))))))/2.
         return psi11, psi22, psi12
-    
-    def _rotation(self, xx, yy, phi):
-        cos_phi, sin_phi = jnp.cos(phi), jnp.sin(phi)
-        xx_rot = cos_phi*xx + sin_phi*yy
-        yy_rot = -sin_phi*xx + cos_phi*yy
-        return xx_rot, yy_rot
 
 def flatten_func(self):
     children = ()
