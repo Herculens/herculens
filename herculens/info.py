54167c9b302140fe9f213435717c74d760ca6716
Aymeric Galan <aymeric.galan@gmail.com>
2025-07-09 13:58:48 +0200

    Ready for minor release 0.2.3

--- herculens/info.py
+++ herculens/info.py
@@ -6,7 +6,7 @@ and provides some basic information about the package.
 """
 
 # Set the package release version
-version_info = (0, 2, 2)
+version_info = (0, 2, 3)
 __version__ = '.'.join(str(c) for c in version_info)
 
 # Set the package details
